fb966e2c4633b045449f5dca3aff4e2a06aea238
Sangkwun <wer2774@gmail.com>
2017-12-27 14:44:23 +0900

    Change admin panel

--- instagram/images/models.py
+++ instagram/images/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from instagram.users import models as user_models
+from django.utils.encoding import python_2_unicode_compatible
 
+@python_2_unicode_compatible
 class TimeStampModel(models.Model):
 
     created_at = models.DateTimeField(auto_now_add=True)
@@ -9,6 +11,7 @@ class TimeStampModel(models.Model):
     class Meta:
         abstract = True
 
+@python_2_unicode_compatible
 class Image(TimeStampModel):
 
     """ Image model  """
@@ -18,17 +21,29 @@ class Image(TimeStampModel):
     caption = models.TextField()
     creator = models.ForeignKey(user_models.User, null=True)
 
+    def __str__(self):
+        return '{} - {}'.format(self.location, self.caption)
+
+@python_2_unicode_compatible
 class Comment(TimeStampModel):
 
     """ Comment model  """
     
     creator = models.ForeignKey(user_models.User, null=True)
-    imgae = models. ForeignKey(Image, null=True)
+    image = models. ForeignKey(Image, null=True)
     message = models.TextField()
 
+    def __str__(self):
+        return self.message
+
+
+@python_2_unicode_compatible
 class Like(TimeStampModel):
 
     """ Like model  """
 
     creator = models.ForeignKey(user_models.User, null=True)
-    imgae = models. ForeignKey(Image, null=True)
+    image = models. ForeignKey(Image, null=True)
+
+    def __str__(self):
+        return 'User: {} Image_caption: {}'.format(self.creator.username, self.image.caption)
