8584f6835126af565254ea6f200c4a4cd06859a2
Sangkwun <wer2774@gmail.com>
2018-01-03 19:05:41 +0900

    create image delete api

--- instagram/images/views.py
+++ instagram/images/views.py
@@ -42,6 +42,14 @@ class Feed(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+        try:
+            image = models.Image.objects.get(id=image_id, creator=user)
+            return image
+
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format=None):
 
         user = request.user
@@ -59,10 +67,9 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id=image_id, creator=user)
+        image = self.find_own_image(image_id, user)
 
-        except models.Image.DoesNotExist:
+        if image is None:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(image, data=request.data, partial=True)
@@ -76,6 +83,21 @@ class ImageDetail(APIView):
         else:
 
             return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+
+
+    def delete(self, request, image_id, format=None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
 class LikeImage(APIView):
