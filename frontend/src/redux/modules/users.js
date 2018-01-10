cff6affd1005e0d5dad13c94186c6e8c4b13ab35
Sangkwun <wer2774@gmail.com>
2018-01-10 10:49:55 +0900

    install redux and react-redux

---
+++ frontend/src/redux/modules/users.js
@@ -0,0 +1,38 @@
+//imports
+
+
+//actions
+
+
+//action creator
+
+
+
+//initial state
+
+const initialState = {
+    isLoggedIn: localStorage.getItem('jwt') || false
+};
+
+
+//reducer
+function reducer(state=initialState, action){
+    switch(action.type){
+        default:
+            return state;
+    }
+}
+
+
+
+//reducer function
+
+
+
+
+//exports
+
+
+
+//reducer export
+export default reducer;
