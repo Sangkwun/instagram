9f6dac9b75f6d616d02544da0faaab50490c7000
Sangkwun <wer2774@gmail.com>
2018-01-12 11:51:32 +0900

    Change Loginform to statecomponent and input control

--- frontend/src/components/LoginForm/container.js
+++ frontend/src/components/LoginForm/container.js
@@ -1,6 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LoginForm from './presenter';
 
-const Container = props => <LoginForm {...props} />;
+
+class Container extends Component{
+    state = {
+        username: '',
+        password: ''
+    };
+
+    render(){
+        const { username, password } = this.state;
+        return <LoginForm 
+            handleInputChange={this._handleInputChangeEvent}
+            handleSubmit={this._handleSubmit}
+            usernameValue={username} 
+            passwordValue={password}/>
+    };
+    _handleInputChangeEvent = event =>{
+        
+        const { target : { value, name }} = event;
+        //const value = event.target;
+        
+        this.setState({
+            [name] : value 
+        })
+    };
+    _handleSubmit = event =>{
+        event.preventDefault();
+        console.log(this.state);
+        //Redux Action
+
+    }
+}
 
 export default Container;
--- frontend/src/components/LoginForm/presenter.js
+++ frontend/src/components/LoginForm/presenter.js
@@ -3,33 +3,28 @@ import Ionicon from "react-ionicons";
 import formStyles from "shared/formStyles.scss";
 import Proptypes from "prop-types";
 
-export const LoginForm = (props, context) => (
-  <div className={formStyles.formComponent}>
-    <form className={formStyles.form}>
-      <input
-        type="text"
-        placeholder={context.t("Username")}
-        className={formStyles.textInput}
-      />
-      <input
-        type="password"
-        placeholder={context.t("Password")}
-        className={formStyles.textInput}
-      />
-      <input
-        type="submit"
-        placeholder={context.t("Log in")}
-        className={formStyles.button}
-      />
-    </form>
-    <span className={formStyles.divider}>or</span>
-    <span className={formStyles.facebookLink}>
-      <Ionicon icon="logo-facebook" fontSize="20px" color="$385185" />
-      {context.t("Log in with Facebook")}
-    </span>
-    <span className={formStyles.forgotLink}>{context.t("Forgot password?")}</span>
-  </div>
-);
+export const LoginForm = (props, context) => <div className={formStyles.formComponent}>
+           <form className={formStyles.form} onSubmit={props.handleSubmit}>
+             <input type="text" placeholder={context.t("Username")} className={formStyles.textInput} value={props.usernameValue} onChange={props.handleInputChange} name="username" />
+             <input type="password" placeholder={context.t("Password")} className={formStyles.textInput} value={props.passwordValue} onChange={props.handleInputChange} name="password" />
+             <input type="submit" placeholder={context.t("Log in")} className={formStyles.button} />
+           </form>
+           <span className={formStyles.divider}>or</span>
+           <span className={formStyles.facebookLink}>
+             <Ionicon icon="logo-facebook" fontSize="20px" color="$385185" />
+             {context.t("Log in with Facebook")}
+           </span>
+           <span className={formStyles.forgotLink}>
+             {context.t("Forgot password?")}
+           </span>
+         </div>;
+
+LoginForm.prototype = {
+  usernameValue: Proptypes.string.isRequired,
+  passwordValue: Proptypes.string.isRequired,
+  handleInputChange: Proptypes.func.isRequired,
+  handleSubmit: Proptypes.func.isRequired
+}
 
 LoginForm.contextTypes = {
   t: Proptypes.func.isRequired
