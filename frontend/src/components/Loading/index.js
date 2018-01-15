c6bdc3544d6d70466085fe21a47e73fa93680173
Sangkwun <wer2774@gmail.com>
2018-01-15 12:48:08 +0900

    add feed and loading component

---
+++ frontend/src/components/Loading/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './styles.scss';
+
+const Loading = props =>{
+    return(
+    <div className = {styles.container}>
+        <img src = {require('images/loading.png')} className={styles.spinner} alt="Loading"/>
+    </div>
+
+    )};
+
+export default Loading;
